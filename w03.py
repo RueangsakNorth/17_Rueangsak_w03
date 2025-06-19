37999099e56559a3ba326f65d87881564ffd4e2f
NorthRueangsak <notdbk@gmail.com>
2025-06-19 15:59:46 +0700

    เพิ่มError Handling ใน BMI Calculator

--- w03.py
+++ w03.py
@@ -1,5 +1,6 @@
 # BMI Calculator
 # Result: BMI = weight / (height ** 2)
+
 try:
      w = float(input("Enter your weight in kg: "))
      if w < 0:
@@ -12,14 +13,13 @@ except ValueError:
 try:
     h = float(input("Enter your height in centimeters: "))
     if h < 0:
-        print("น้ำหนักต้องเป็นจำนวนบวก")
+        print("ส่วนสูงต้องเป็นจำนวนบวก")
     else:
         print(f"น้ำหนักที่ตั้งค่า: {h} cm")
 except ValueError:
     print("กรุณาใส่ตัวเลขเท่านั้น")
     
-   
 # Convert height from centimeters to meters 
 r = w / ((h/100) ** 2)
 # Print the result
-# print(f"Your BMI is: {r:.2f}")
+print(f"Your BMI is: {r:.2f}")
